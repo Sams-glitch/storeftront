bfea46cd3fe98ec16cdc7922a573d6e62a0bff68
Victor Kamlomo <kamlomovictor@gmail.com>
2022-11-25 11:10:26 +0200

    banner added

---
+++ modules/banners/Banner.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react';
+
+function Banner() {
+  return <div className="h-52 rounded md:h-52 bg-slate-700 w-full"></div>;
+}
+
+export default Banner;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Banner from '../modules/banners/Banner'
 import Layout from '../modules/layout/Layout'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <Layout>
-      <h1 className='text-4xl'>ogogog</h1>
+      <Banner />
     </Layout>
   )
 }
