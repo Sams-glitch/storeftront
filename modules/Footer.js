b7e3e3e7bde017b51c39f1ebb56452b0f859465a
Victor Kamlomo <kamlomovictor@gmail.com>
2022-11-25 15:35:35 +0200

    footer

--- modules/Footer.js
+++ modules/Footer.js
@@ -1,10 +1,36 @@
+import { ShoppingBagIcon } from '@heroicons/react/24/outline';
 import React from 'react';
 
 export default function Footer() {
   return (
-    <footer>
-      <div className="w-full md:w-3/4 mx-auto py-4 md:space-x-3">
+    <footer className="border-t w-full">
+      <div className="w-full md:w-3/4 mx-auto py-8 md:space-x-3">
         {/* footer */}
+        <div className="grid grid-flow-row-dense grid-cols-2 md:grid-cols-5 gap-y-5 gap-x-6 w-full">
+          <div className="col-span-2 md:col-span-1 inline-flex items-start">
+            <span>
+              <ShoppingBagIcon className="h-10 w-10" />
+              <h2 className="text-xl">Store</h2>
+            </span>
+            <span className="invisible"></span>
+          </div>
+          <div>
+            <ul className='flex flex-col items-start space-y-3'>
+              <li>About us</li>
+              <li>Terems of use</li>
+              <li>Shipping</li>
+              <li>Returns</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Privancy policy</li>
+            </ul>
+          </div>
+        </div>
+        <div className="md:mt-8 border-t">
+          <h1 className="py-6">2022 Store, All rights reserved</h1>
+        </div>
       </div>
     </footer>
   );
--- modules/layout/Header.js
+++ modules/layout/Header.js
@@ -6,11 +6,11 @@ function Header() {
   return (
     <>
       <header className="md:w-3/4 mx-auto py-4 px-3 md:space-x-3 sticky">
-        <div className="grid grid-flow-row-dense sm:grid-cols-2 md:grid-cols-7 gap-y-4 sm:gap-x-3 md:gap-x-12 text-emerald-700 mt-3 w-full">
+        <div className="grid grid-flow-row-dense sm:grid-cols-2 md:grid-cols-7 gap-y-4 sm:gap-x-3 md:gap-x-12 text-emerald-800 mt-3 w-full">
           <div className="col-span-1">
-            <div className="text-left inline-flex items-center">
-              <ShoppingBagIcon className="h-8 w-8" />
-              <h1 className="sm:text-sm md:text-md font-title">Store.</h1>
+            <div className="text-left inline-flex items-end">
+              <ShoppingBagIcon className="h-8 w-8 sr-only" />
+              <h1 className="sm:text-sm md:text-md font-medium">Store.</h1>
             </div>
           </div>
           <div className="col-span-2 md:col-span-5">
@@ -27,13 +27,13 @@ function Header() {
           <div className="block ml-auto">
             <div className="inline-flex items-center space-x-3">
               <h3 className="text-sm underline">Account</h3>
-              <ShoppingBagIcon className="h-6 w-6 text-md font-extrabold" />
+              <ShoppingBagIcon className="h-7 w-7 font-extrabold" />
             </div>
           </div>
         </div>
 
         <div className="mt-3 w-full show-for-small hidden md:block">
-          <nav className="inline-flex space-x-3 text-sm font-medium text-emerald-700 tracking-tight md:-ml-2">
+          <nav className="inline-flex space-x-3 text-sm font-semibold text-emerald-800 tracking-tight uppercase md:-ml-2">
             <Link href={'/'}>All</Link>
             <Link href={'/'}>Category 1</Link>
             <Link href={'/'}>Category 2</Link>
